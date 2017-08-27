2d09f3a94d3571c2b071b86fb0a98c4574862d86
Karan Joshi <karanjoshi31@live.com>
2017-08-26 18:09:15 -0700

    Added page navigation from clicking the buttons!

---
+++ app/src/main/java/com/pantrychefapp/pantrychef/MyPantry.java
@@ -0,0 +1,17 @@
+package com.pantrychefapp.pantrychef;
+
+
+/*
+  Created by Karan on 8/26/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPantry extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_pantry);
+    }
+}
---
+++ app/src/main/java/com/pantrychefapp/pantrychef/MyRecipes.java
@@ -0,0 +1,18 @@
+package com.pantrychefapp.pantrychef;
+
+/*
+  Created by Karan on 8/26/2017.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class MyRecipes extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_recipes);
+    }
+}
