efa8809ae1855bb5f3162a1e6de5353e0b920531
Brandon Schabell <brandonschabell@gmail.com>
2017-08-31 23:47:35 -0500

    ISSUE-8 created addRecipe activity

--- app/src/main/java/com/pantrychefapp/pantrychef/MyRecipes.java
+++ app/src/main/java/com/pantrychefapp/pantrychef/MyRecipes.java
@@ -1,18 +1,19 @@
 package com.pantrychefapp.pantrychef;
 
-/*
-  Created by Karan on 8/26/2017.
- */
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.view.View;
 
 public class MyRecipes extends AppCompatActivity {
-
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_recipes);
     }
+
+	public void addRecipe(View view) {
+		Intent intent = new Intent(this, AddRecipeActivity.class);
+		startActivity(intent);
+	}
 }
